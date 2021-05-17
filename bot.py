074b9b7c098827c58f1265e81b041b19ceb39ae6
CYRIL C KURIAN <74858827+cyrilckurian@users.noreply.github.com>
2021-05-17 17:43:38 +0530

    Add files via upload
    twitter bot

---
+++ bot.py
@@ -0,0 +1,53 @@
+import tweepy
+import time
+
+print("This is My twitter bot")
+
+CONSUMER_KEY = 'AVJSguK1UVMf3SklB7zbk077S'
+CONSUMER_SECRET = 'Ae3hJSSTx1EQgeSbn6JQbsTkoflZT5qURoBX08geT4LhtTF46F'
+ACCESS_KEY = '1261596317211668480-ebuPEf6dpIVlGO69q2DzzSTB4xhdjC'
+ACCESS_SECRET = 'yCVkRxIZoVmj5Ol3FV0lepual9vlZQQtyLn8vdDfqFqid'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+#mentions = api.mentions_timeline()
+
+#for mention in mentions:
+#	print(str(mention.id) + ' - ' + mention.text)
+#	if '#helloworld' in mention.text.lower():
+#		print('found #helloworld!')
+#		print('responding back...')
+
+FILE_NAME = 'last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+	f_write = open(file_name, 'w')
+	f_write.write(str(last_seen_id))
+	f_write.close()
+	return
+
+def reply_to_tweets():
+	print('retrieving and replying to tweets...')
+	last_seen_id = retrieve_last_seen_id(FILE_NAME)
+	mentions = api.mentions_timeline(last_seen_id, tweet_mode='extended')
+
+	for mention in reversed(mentions):
+		print(str(mention.id) + ' - ' + mention.full_text)
+		last_seen_id = mention.id
+		store_last_seen_id(last_seen_id, FILE_NAME)
+		if '#helloworld' in mention.full_text.lower():
+			print('found #helloworld!')
+			print('responding back...')
+			api.update_status('@' + mention.user.screen_name + '#HelloWorld back to you!', mention.id)
+
+while True:
+	reply_to_tweets()
+	time.sleep(2)
